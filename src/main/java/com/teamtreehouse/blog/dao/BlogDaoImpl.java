e8151bc6ead2422b0a4fa0926bd070cb106f193a
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-03-24 04:14:12 -0300

    added the DAO Implementation

--- src/main/java/com/teamtreehouse/blog/dao/BlogDaoImpl.java
+++ src/main/java/com/teamtreehouse/blog/dao/BlogDaoImpl.java
@@ -2,6 +2,7 @@ package com.teamtreehouse.blog.dao;
 
 import com.teamtreehouse.blog.model.BlogEntry;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,14 +10,21 @@ import java.util.List;
  */
 public class BlogDaoImpl implements BlogDao{
 
-    @Override
-    public boolean addEntry(BlogEntry blogEntry) {
-        return false;
+    private List<BlogEntry> entries;
+
+    public BlogDaoImpl() {
+        this.entries = new ArrayList<>();
     }
 
     @Override
+    public boolean addEntry(BlogEntry blogEntry) {
+        return entries.add(blogEntry);
+    }
+
+
+    @Override
     public List<BlogEntry> findAllEntries() {
-        return null;
+        return new ArrayList<>(entries);
     }
 
     @Override
