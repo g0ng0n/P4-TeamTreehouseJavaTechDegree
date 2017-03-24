6089f612a6ce4c4ed480c890a1441ca9c6351c03
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-03-24 04:13:52 -0300

    added the model implementation

--- src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
+++ src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
@@ -1,8 +1,78 @@
 package com.teamtreehouse.blog.model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class BlogEntry {
+
+    private String title;
+    private String creator;
+    private Set<Comment> comments;
+    private String entry;
+
+    public BlogEntry(String title, String creator, String entry) {
+        this.title = title;
+        this.creator = creator;
+        this.entry = entry;
+
+        comments = new HashSet<>();
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BlogEntry blogEntry = (BlogEntry) o;
+
+        if (title != null ? !title.equals(blogEntry.title) : blogEntry.title != null) return false;
+        return creator != null ? creator.equals(blogEntry.creator) : blogEntry.creator == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (creator != null ? creator.hashCode() : 0);
+        return result;
+    }
+
     public boolean addComment(Comment comment) {
         // Store these comments!
-        return false;
+        return comments.add(comment);
+    }
+
+    //Getters and setters
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public String getEntry() {
+        return entry;
+    }
+
+    public void setEntry(String entry) {
+        this.entry = entry;
+    }
+
+    private List<Comment> getComments(){
+        return new ArrayList<>(comments);
+
     }
 }
--- src/main/java/com/teamtreehouse/blog/model/Comment.java
+++ src/main/java/com/teamtreehouse/blog/model/Comment.java
@@ -1,4 +1,22 @@
 package com.teamtreehouse.blog.model;
 
 public class Comment {
+    private String name;
+    private String comment;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
