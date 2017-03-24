adffcad09fee28f7d8a777a676ded261ac2fe7ed
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-03-24 02:53:21 -0300

    first commit

---
+++ src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
@@ -0,0 +1,8 @@
+package com.teamtreehouse.blog.model;
+
+public class BlogEntry {
+    public boolean addComment(Comment comment) {
+        // Store these comments!
+        return false;
+    }
+}
